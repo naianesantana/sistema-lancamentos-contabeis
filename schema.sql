ab9decef4135e1cac954a97189bd5a20c0a02171
naianesantana <nextgoriginal@gmail.com>
2026-02-22 14:03:17 -0300

    :broom: style(db): limpeza do script de schema e melhoria na legibilidade

--- schema.sql
+++ schema.sql
@@ -1,22 +1,56 @@
-CREATE DATABASE LANCAMENTOSCONTABEIS;
-USE LANCAMENTOSCONTABEIS;
+CREATE DATABASE  IF NOT EXISTS `lancamentoscontabeis` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `lancamentoscontabeis`;
 
-CREATE TABLE Conta (
-  conta_id INTEGER UNSIGNED NOT NULL,
-  descricao VARCHAR(50) NOT NULL,
-  PRIMARY KEY(id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE Lancamento (
-  lancamento_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  data_registro DATE NOT NULL,
-  contaDebito_id INTEGER UNSIGNED NOT NULL,
-  contaCredito_id INTEGER UNSIGNED NOT NULL,
-  valor DECIMAL(19,2) NOT NULL,
-  historico VARCHAR(200) NOT NULL,
-  PRIMARY KEY(lancamento_id),
-  FOREIGN KEY(contaDebito_id) references Conta(conta_id),
-  FOREIGN KEY(contaCredito_id) references Conta(conta_id),
-  CONSTRAINT check_contas_diferentes CHECK(contaDebito_id <> contaCredito_id),
-  CONSTRAINT valor_maior_zero CHECK(valor > 0)
-);
+
+-- Table structure for table `contas`
+DROP TABLE IF EXISTS `contas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contas` (
+  `conta_id` int unsigned NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  PRIMARY KEY (`conta_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+-- Table structure for table `lancamentos`
+DROP TABLE IF EXISTS `lancamentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lancamentos` (
+  `lancamento_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `data_registro` date NOT NULL,
+  `contaDebito_id` int unsigned NOT NULL,
+  `contaCredito_id` int unsigned NOT NULL,
+  `valor` decimal(19,2) NOT NULL,
+  `historico` varchar(200) NOT NULL,
+  PRIMARY KEY (`lancamento_id`),
+  KEY `contaDebito_id` (`contaDebito_id`),
+  KEY `contaCredito_id` (`contaCredito_id`),
+  CONSTRAINT `lancamentos_ibfk_1` FOREIGN KEY (`contaDebito_id`) REFERENCES `contas` (`conta_id`),
+  CONSTRAINT `lancamentos_ibfk_2` FOREIGN KEY (`contaCredito_id`) REFERENCES `contas` (`conta_id`),
+  CONSTRAINT `check_contas_diferentes` CHECK ((`contaDebito_id` <> `contaCredito_id`)),
+  CONSTRAINT `valor_maior_zero` CHECK ((`valor` > 0))
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
